02263b4558615efbd1b04148219d1ea19f4954ce
Caleb Perkins <calebtperkins@gmail.com>
2014-03-03 01:19:34 -0800

    Completed problem 53

---
+++ src/53-longest-increasing-sub-seq.clj
@@ -0,0 +1,13 @@
+(fn [xs]
+  (first (reduce
+          (fn [[best current] n]
+            (if (empty? current)
+              [best [n]]
+              (if (= n (inc (last current)))
+                (let [current' (conj current n)]
+                  (if (> (count current') (count best))
+                    [current' current']
+                    [best current']))
+                [best [n]])))
+          [[], []]
+          xs)))
